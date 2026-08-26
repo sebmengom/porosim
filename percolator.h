320cf742d3a46b139f62e3f335be46410d79167e
sebmengom <Sebmengom@gmail.com>
2026-08-26 13:27:26 -0600

    BUGS FIXED: Privatized  int bottom{};   unionFind arr;   std::vector<int> gateStatus; in percolator.h, no that no one can modify that data without previously running the percolator functions.

--- percolator.h
+++ percolator.h
@@ -5,22 +5,21 @@
 #include <vector>
 
 class percolator {
+private:
+  int n{};
+  int top{};
+  int bottom{};
+  unionFind arr;
+  std::vector<int> gateStatus;
+
 public:
   percolator(int n);
-  unionFind arr{(percolator::n * percolator::n) + 2};
-  std::vector<int> gateStatus =
-      std::vector<int>(percolator::n * percolator::n, 0);
 
   void openGate(int row, int col);
 
   bool isOpen(int row, int col);
 
   bool percolates();
-
-private:
-  int n{};
-  int top{};
-  int bottom{};
 };
 
 #endif // !PERCOLATOR_H
