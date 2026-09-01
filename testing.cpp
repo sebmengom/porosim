610d12e36594a7244b1ed8392f01c8042545a56e
sebmengom <Sebmengom@gmail.com>
2026-09-01 15:45:34 -0600

    untrack testing.cpp

--- testing.cpp
+++
@@ -1,9 +0,0 @@
-
-#include <iostream>
-#include <random>
-int main() {
-  std::random_device rd;
-  std::mt19937 machine{rd()};
-  std::uniform_real_distribution<double> distribution(0, 1);
-  std::cout << machine;
-}
