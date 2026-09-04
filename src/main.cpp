cc7bb5fa1ac2a6597bf191b8fb2f411410b74752
sebmengom <Sebmengom@gmail.com>
2026-09-03 20:14:42 -0600

    added try catch cases to ensure input validation

--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,35 @@
 #include <string>
 
 int main(int argc, char *argv[]) {
+
   if (argc == 3) {
-    int gridSize{std::stoi(argv[1])};
-    int numberOfTrials{std::stoi(argv[2])};
+    try {
+      int gridSize{std::stoi(argv[1])};
+      int numberOfTrials{std::stoi(argv[2])};
 
-    montecarlo sim(gridSize, numberOfTrials);
-    auto startTest = std::chrono::steady_clock::now();
-    sim.newTrials();
-    auto endTest = std::chrono::steady_clock::now();
-    auto elapsedTest = std::chrono::duration_cast<std::chrono::milliseconds>(
-        endTest - startTest);
-    sim.printNewResults(sim.newResults);
-    std::cout << "Time elapsed: " << elapsedTest.count() << '\n';
+      montecarlo sim(gridSize, numberOfTrials);
+      auto startTest = std::chrono::steady_clock::now();
+      sim.newTrials();
+      auto endTest = std::chrono::steady_clock::now();
+      auto elapsedTest = std::chrono::duration_cast<std::chrono::milliseconds>(
+          endTest - startTest);
+      sim.printNewResults(sim.newResults);
+      std::cout << "Time elapsed: " << elapsedTest.count() << '\n';
 
-    montecarlo injectionSim(gridSize, numberOfTrials);
-    startTest = std::chrono::steady_clock::now();
-    injectionSim.injectionTrials();
-    endTest = std::chrono::steady_clock::now();
+      montecarlo injectionSim(gridSize, numberOfTrials);
+      startTest = std::chrono::steady_clock::now();
+      injectionSim.injectionTrials();
+      endTest = std::chrono::steady_clock::now();
 
-    elapsedTest = std::chrono::duration_cast<std::chrono::milliseconds>(
-        endTest - startTest);
+      elapsedTest = std::chrono::duration_cast<std::chrono::milliseconds>(
+          endTest - startTest);
 
-    sim.printNewResults(injectionSim.injectionResults);
-    std::cout << "Time elapsed: " << elapsedTest.count() << '\n';
-
+      sim.printNewResults(injectionSim.injectionResults);
+      std::cout << "Time elapsed: " << elapsedTest.count() << '\n';
+    } catch (...) {
+      std::cout << "Invalid Input.\n";
+      return 1;
+    }
     return 0;
   } else {
     std::cout << "RUN BY: ./'executable' #gridSize INT #numberOfTrials INT"
